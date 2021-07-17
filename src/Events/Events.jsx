450d3d2eaa4dc5731c29b3b58e3320cd922bcd8d
Dylan Lotivio <dylan.lotivio@autodesk.com>
2021-07-16 23:51:05 -0700

    Fix typos

--- src/Events/Events.jsx
+++ src/Events/Events.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 const Events = () => {
   return <div className="heroContainer">
     <div className="container">
-      <h4><a href="https://forms.gle/2KV8UBqoPi1yQZG47">Sign Up</a></h4>
+      <h3><a href="https://forms.gle/2KV8UBqoPi1yQZG47" target="_blank" rel="noreferrer">Sign Up</a></h3>
 
       <p>
         Have something that you want to share? Wanna have a movie night, a beer tasting,
@@ -16,15 +16,15 @@ const Events = () => {
         Performers should be sure to bring all other necessary equipment i.e: mics, cables, etc.
       </p>
 
-      <h4>
-        <a href="https://docs.google.com/spreadsheets/d/1ijB63vLOuqGR3PhSQp8mVYjYC15Ndn8tXvNM43XtWNU">
+      <h3>
+        <a href="https://docs.google.com/spreadsheets/d/1ijB63vLOuqGR3PhSQp8mVYjYC15Ndn8tXvNM43XtWNU" target="_blank" rel="noreferrer">
           Event Schedule
         </a>
-      </h4>
+      </h3>
 
       <p>
         We will also have a physical board where campers can add spontaneous events at the camp
-        itself. The board will list the events that are currently on the sign up sheet as well
+        itself. The board will list the events that are currently on the sign up sheet as well.
       </p>
     </div>
   </div>;
