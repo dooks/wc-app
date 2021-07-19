3f552dc576941c8c7f338391e85542df807aeab8
Dylan Lotivio <dylan.lotivio@autodesk.com>
2021-07-18 23:22:08 -0700

    * Code splitting * Manifest updates * bootstrap icon * Footer fixes

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,16 +4,16 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear().toString();
-  return <div className={styles.footer}>
+  return <>
     <hr />
-    <div>
-      <span>Woodland Critters</span>
-      <span> {currentYear}</span>
-    </div>
-    <div>
+    <div className={styles.footer}>
       <a href="mailto:info@woodlandcritters.net">Contact Us</a>
+      <div>
+        <span>Woodland Critters</span>
+        <span> {currentYear}</span>
+      </div>
     </div>
-  </div>;
+  </>;
 };
 
 export default Footer;
