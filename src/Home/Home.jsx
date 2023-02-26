44d553568b1cb302172b7646cecb1da99ad379ee
Ziggy <dook@stardust.red>
2023-02-25 19:51:18 -0800

    Update language, fix GH pages

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -8,8 +8,16 @@ const Home = (props) => {
   return <>
     <div className={styles.hero}>
       <h1 className={styles.title}>Woodland Critters</h1>
-      <h2>Donner Pass, California</h2>
       <h2>June 15th - 19th, 2023</h2>
+
+      <p style={{
+        textAlign: "center",
+        width: "450px",
+        fontSize: "1.2em"
+      }}>
+        Woodland Critters is a private outdoor furry event located in the mountains of Donner Pass.
+      </p>
+
       <Button
         className={styles.registerBtn}
 
@@ -22,20 +30,6 @@ const Home = (props) => {
         onClick={() => props.selectKey("register")}>
         Register
       </Button>
-    </div>
-
-    <div className={classnames("container", styles.description)}>
-      <p>
-        Woodland Critters is a private outdoor furry event inspired by Campfire Tails and Burning Man.
-        Hosted in the mountains of Donner Pass, we provide 75 acres of forest to camp in, with access
-        roads, basic amenities and an outdoor kitchen.
-      </p>
-
-      <p>
-        We abide by the <a href="https://burningman.org/about/10-principles/" target="_blank" rel="noreferrer">
-          Burning Man 10 Principles.
-        </a>
-      </p>
     </div>
   </>
 };
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/wc-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
